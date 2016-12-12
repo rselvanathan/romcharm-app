d1067463360ae933f5386b37246f0ce640b2c7fb
Romesh Selvanathan <Romesh.Selvanathan@thomsonreuters.com>
2016-12-12 09:57:54 +0000

    Fixed issue with Chrome and Mozilla

--- src/testController.js
+++ src/testController.js
@@ -7,6 +7,12 @@ app.controller('testController', function($scope) {
     $scope.formData = {rsvpName : ""};
 
     $scope.buttonClick = function() {
+        // $http({
+        //     url:"http://localhost:8080/families/{$scope.formData.rsvpName}",
+        //     method:"GET"
+        // }).then(function(response) {
+        //     $scope.value = response.data.familyName;
+        // });
         $scope.value = $scope.formData.rsvpName;
     }
 });
