88e1387d4a7f21d6186e8f7c69b27d577e92c30f
Romesh Selvanathan <Romesh.Selvanathan@thomsonreuters.com>
2016-12-14 15:19:38 +0000

    Registration Number of Attending field will now disable and enable dynamically with preset values

--- src/js/registerController.js
+++ src/js/registerController.js
@@ -2,4 +2,16 @@ var RegisterController = function($scope) {
     $scope.form = {
         attendance : 'yesOption'
     };
+
+    $scope.isAttending = function() {
+        if($scope.form.attendance === 'yesOption')
+            return false;
+        else return true;    
+    };
+
+    $scope.setValue = function() {
+        if($scope.form.attendance === 'yesOption')
+            return 1;
+        else return 0;  
+    };
 };
