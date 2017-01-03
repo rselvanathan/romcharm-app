4ace87e6434353e82b836a6eb1d4b6b472ca5fba
Romesh Selvanathan <Romesh.Selvanathan@thomsonreuters.com>
2017-01-03 15:37:08 +0000

    Added Deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker pull rselvanathan/romcharm-app:latest
+isImageRunning=$(docker inspect -f {{.State.Running}} romcharm-app 2> /dev/null)
+if [ "$isImageRunning" = "true" ]; then
+	echo "Removing romcharm-app container"
+	docker stop romcharm-app
+	docker rm romcharm-app
+fi
+value=$(docker images -q --filter "dangling=true")
+if [ "$value" = "" ]; then
+	echo "No Dangling Images"
+else
+	echo "Removing Dangling Images"
+ 	docker images -q --filter "dangling=true" | xargs docker rmi
+fi
+docker run -d -e VIRTUAL_HOST=www.romandcharmi.com,romandcharmi.com --name romcharm-app -it rselvanathan/romcharm-app:latest
