bb2755a61564fb1d91e0ca866c511d9cd3f68878
Romesh <rselvanathan@omnifone.com>
2017-01-22 23:24:11 +0000

    Removing http/https from links to make the script more secure

--- deploy-romcharm-app.sh
+++ deploy-romcharm-app.sh
@@ -12,4 +12,4 @@ else
 	echo "Removing Dangling Images"
  	docker images -q --filter "dangling=true" | xargs docker rmi
 fi
-docker run -d -e VIRTUAL_HOST={URLS} --name romcharm-app -it rselvanathan/romcharm-app:latest
+docker run -d -e VIRTUAL_HOST={URLS} -e LETSENCRYPT_HOST= -e LETSENCRYPT_EMAIL= --name romcharm-app -it rselvanathan/romcharm-app:latest
